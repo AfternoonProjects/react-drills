50c8c0ada6ead1b629f91acf0b0e3899105eafe9
Andrew Montgomery <wamontgomery0068@gmail.com>
2018-12-02 08:48:17 -0600

    Completed

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,18 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      footballTeams: [
+        "New Orleans Saints",
+        "Tennessee Titans",
+        "Denver Broncos",
+        "Atlanta Falcons",
+        "Greenbay Packers"
+      ]
+    }
+  }
+
   render() {
+    let teamsToDisplay = this.state.footballTeams.map( (element,index) => {
+      return (
+        <h2 key={ index }>{ element }</h2>
+      )
+    })
+
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        { teamsToDisplay }  
       </div>
     );
   }
