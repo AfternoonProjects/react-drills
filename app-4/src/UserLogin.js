8dda0ead3c7baf5959cf92f9610b79c772634819
Andrew Montgomery <wamontgomery0068@gmail.com>
2018-12-02 12:41:16 -0600

    app-4 Completed

---
+++ app-4/src/UserLogin.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+
+class UserLogin extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            username: "",
+            password: ""
+        }
+    };
+
+    handleChangeUsername( name ) {
+        this.setState ( {username: name} );
+    };
+
+    handleChangePassword( pass ) {
+        this.setState ( {password: pass} );
+    };
+
+    handleLogin() {
+        alert( `Username: ${ this.setState.username } Password: ${ this.state.password }` )
+    };
+
+    render() {
+        return (
+            <div className = "User_Section">
+
+                <input onChange={(e) => this.handleChangeUsername( e.target.value ) } type="text" />
+
+                <input onChange={(e) => this.handleChangePassword( e.target.value ) } type="text" />
+
+                <button onClick={ this.handleLogin }>Login</button>
+
+            </div>
+        )
+    }
+};
+
+export default UserLogin;
