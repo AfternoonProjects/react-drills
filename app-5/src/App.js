9f84c5617f5c1fd985e91e76d06e95a2e8175e72
Andrew Montgomery <wamontgomery0068@gmail.com>
2018-12-02 12:58:44 -0600

    App-5 Completed

--- app-5/src/App.js
+++ app-5/src/App.js
@@ -1,18 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Image from './Image';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Image myImage={ "https://www.gannett-cdn.com/-mm-/0f62eff2e6b466eb911e109fc511c8b75c98f3e7/c=121-217-2193-1775/local/-/media/2017/11/09/USATODAY/USATODAY/636458319807256912-USP-NFL-Tampa-Bay-Buccaneers-at-New-Orleans-Saint.jpg?width=534&height=401&fit=crop" } />
       </div>
     );
   }
